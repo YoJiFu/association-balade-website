27c43ceca1d9270b16ec1828e4ef1cad61afb7bb
bzhazreal <stanislas.david@mailoo.org>
2018-09-08 18:52:03 +0200

    Add ballade_theme theme

---
+++ app/themes/custom/ballade_theme/config.rb
@@ -0,0 +1,12 @@
+# Inject variables on SASS
+# require 'bootstrap-sass'
+# require 'compass/import-once/activate'
+
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'scss'
+images_dir = 'img'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = true
