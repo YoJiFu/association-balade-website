82cce541654d1684227bc47bcd2c800da536e5c4
bzhazreal <stanislas.david@protonmail.com>
2020-02-26 00:26:08 +0100

    Add missing brace

--- script/core_lib/composer.sh
+++ script/core_lib/composer.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 function install_dev_dependancies() {
-  cd $DRUPAL_DOCROOT && composer install 
+  cd ${DRUPAL_DOCROOT} && composer install 
 }
 
 function install_prod_dependancies() {
-  cd $DRUPAL_DOCROOT && composer install --no-dev
+  cd ${DRUPAL_DOCROOT} && composer install --no-dev
 }
