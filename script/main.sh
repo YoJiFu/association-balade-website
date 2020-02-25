f536bbee2a50f11e05d764d6ffe7565b1335e2b4
bzhazreal <stanislas.david@protonmail.com>
2020-02-26 00:12:49 +0100

    Add comment in main.sh file

--- script/main.sh
+++ script/main.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
+###
+# Entrypoint script
+#
+# The script autoload all files in core_lib && extension_lib directory,
+# and execute the command passed as an argument.
+###
+
+# Bash script autoloader.
 function autoload_bash() {
   for f in $1/*; do
     source $f
   done
 }
 
+# Bash script directories
 DIRECTORY=(
   "/script/core_lib"
   "/script/extension_lib"
 )
 
+# Loader
 for dir in $DIRECTORY;do
   autoload_bash ${dir}
 done
+
+###
+# Execute command passed as argument.
+###
+$1
