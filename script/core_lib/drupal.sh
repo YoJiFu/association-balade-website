42c9195c82bbe6b5217fabee98547215493ed507
bzhazreal <stanislas.david@protonmail.com>
2020-02-29 11:45:18 +0100

    WIP install command

--- script/core_lib/drupal.sh
+++ script/core_lib/drupal.sh
@@ -2,9 +2,23 @@
 
 export PATH=$PATH:/root/.composer/vendor/bin
 
+function update_admin_password() {
+  drush user:password admin "admin"
+}
+
 function install_site_dev() {
-  cd ${DRUPAL_DOCROOT}
-  chmod +w ${DRUPAL_DOCROOT}/sites/default
-  chmod +w ${DRUPAL_DOCROOT}/sites/default/*
-  drush site:install --existing-config
+  echo "WIP"
+  # cd ${DRUPAL_DOCROOT}
+  # chmod +w ${DRUPAL_DOCROOT}/sites/default
+  # chmod +w ${DRUPAL_DOCROOT}/sites/default/*
+  # drush site:install standard
+  # update_admin_password
+  # sed -i '/config_sync_directory/d' ${DRUPAL_DOCROOT}/sites/default/settings.php
+  # echo "\$settings['config_sync_directory'] = '/config/yml/sync/';" >> ${DRUPAL_DOCROOT}/sites/default/settings.php
+  # drush cim -y
+}
+
+function uninstall_site() {
+  drush sql:drop -y
+  rm -rfv ${DRUPAL_DOCROOT}/sites/default/settings.php
 }
